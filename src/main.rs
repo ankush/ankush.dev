2ad09c460ca0ac0366f16e2f174e9d714e11e55b
Ankush Menat <ankushmenat@gmail.com>
2024-04-28 20:12:54 +0530

    feat: basic posts list and view

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
-use axum::extract::State;
+use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::{response::Html, routing::get, Router};
 use minijinja::{context, Environment};
+use serde::Serialize;
+use std::fs::{self, DirEntry};
 use std::sync::Arc;
 
 struct AppState {
     env: Environment<'static>,
+    posts: Vec<Post>,
 }
+
+const POSTS_DIR: &str = "./content/posts/";
 
 #[tokio::main]
 async fn main() {
     let env = get_jenv();
+    let posts = read_posts();
 
-    let app_state = Arc::new(AppState { env });
+    let app_state = Arc::new(AppState { env, posts: posts });
 
     let app = Router::new()
         .route("/", get(homepage))
-        .route("/p", get(get_posts))
+        .route("/p/:slug", get(get_posts))
         .with_state(app_state);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
@@ -35,30 +43,59 @@ fn get_jenv() -> Environment<'static> {
     env
 }
 
+fn read_posts() -> Vec<Post> {
+    let post_files = fs::read_dir(POSTS_DIR).expect("Invalid content directory");
+    post_files
+        .map(|file| file.unwrap().try_into().unwrap())
+        .collect()
+}
+
 async fn homepage(State(state): State<Arc<AppState>>) -> Result<Html<String>, StatusCode> {
     let template = state.env.get_template("home").unwrap();
 
     let rendered = template
         .render(context! {
-            title => "Ankush Menat",
-            welcome_text => "Hello World!",
+            title => "Ankush Menat's Blog",
+            posts => state.posts,
         })
         .unwrap();
 
     Ok(Html(rendered))
 }
 
-async fn get_posts(State(state): State<Arc<AppState>>) -> Result<Html<String>, StatusCode> {
+async fn get_posts(
+    State(state): State<Arc<AppState>>,
+    Path(slug): Path<String>,
+) -> Result<Html<String>, StatusCode> {
     let template = state.env.get_template("post").unwrap();
 
-    let some_example_entries = vec!["Data 1", "Data 2", "Data 3"];
+    let post = state.posts.iter().find(|p| p.slug == slug);
+    match post {
+        Some(post) => {
+            let rendered = template
+                .render(context! {
+                    post => post,
+                })
+                .unwrap();
 
-    let rendered = template
-        .render(context! {
-            title => "Content",
-            entries => some_example_entries,
-        })
-        .unwrap();
+            Ok(Html(rendered))
+        }
+        None => Err(StatusCode::NOT_FOUND),
+    }
+}
 
-    Ok(Html(rendered))
+#[derive(Debug, Serialize)]
+struct Post {
+    slug: String,
+    raw_content: String,
+}
+
+impl From<DirEntry> for Post {
+    fn from(value: DirEntry) -> Self {
+        let file_path = value.file_name().to_str().unwrap().to_string();
+        let slug = file_path.strip_suffix(".md").unwrap().to_string();
+        let raw_content = fs::read_to_string(value.path()).expect("Content should be present in file");
+
+        Post { slug, raw_content }
+    }
 }
