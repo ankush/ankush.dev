3b56f659293bd1381617874ce443f2b5183314ee
Ankush Menat <ankushmenat@gmail.com>
2024-11-17 17:42:58 +0530

    perf: faster compile times, less garbage

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,7 @@ cd ~/projects/ankush_dev
 git fetch origin develop
 git reset --hard origin/develop
 docker compose pull
-docker compose up -d --force-recreate --remove-orphans
-docker image prune -f
+docker compose stop
+docker compose rm -f backend
+docker compose up -d
+docker system prune
